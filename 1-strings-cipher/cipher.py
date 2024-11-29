101a8c66e8e7ec7965b8c67818f64e0c69366d9c
Maxim <maxim271989@gmail.com>
2024-11-29 18:38:41 +0300

    challenge completed

--- 1-strings-cipher/cipher.py
+++ 1-strings-cipher/cipher.py
@@ -1,4 +1,4 @@
-text = 'Hello Zaira!'
+text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'python'
 
 def vigenere(message, key, direction=1):
@@ -23,13 +23,14 @@ def vigenere(message, key, direction=1):
             final_message += alphabet[new_index]
     
     return final_message
+
 def encrypt(message, key):
     return vigenere(message, key)
     
 def decrypt(message, key):
     return vigenere(message, key, -1)
-    
-encryption = encrypt(text, custom_key)
-print(encrypt)
-decryption = decrypt(encryption, custom_key)
-print(decrypt)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
